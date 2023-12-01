e02407d365f70fcb79403b4cca9d776355db86d4
Anu <irikanu@gmail.com>
2023-12-01 14:52:39 +0200

    ANU LoginView algus

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from "@/views/LoginView.vue";
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView
+
   },
   {
     path: '/about',
---
+++ src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="input-group flex-nowrap">
+      <span class="input-group-text" id="addon-wrapping">E-maili aadress</span>
+      <input type="text" class="form-control" placeholder="Username" aria-label="Username"
+             aria-describedby="addon-wrapping">
+    </div>
+    <label for="inputPassword5" class="form-label">Password</label>
+    <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+    <div id="passwordHelpBlock" class="form-text">
+      Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView.vue"
+}
+</script>
