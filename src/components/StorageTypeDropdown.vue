82dc27e62800526868c8f1408cfa723f7b88dca2
AK <a@b.com>
2023-12-06 11:41:30 +0200

    FES-2 wip

---
+++ src/components/StorageTypeDropdown.vue
@@ -0,0 +1,11 @@
+<template>
+  <select class="form-select" aria-label="Tüüp">
+    <option selected value="0">Vali tüüp</option>
+    <option value="1">One</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'StorageTypeDropdown'
+}
+</script>
--- src/views/StorageDetailsView.vue
+++ src/views/StorageDetailsView.vue
@@ -2,59 +2,86 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col col-8">
-        <h1>Rendipind {{storageId}}</h1>
+        <h1>{{ storageName }}</h1>
+        <br>
+        <br>
       </div>
     </div>
 
     <div class="row">
       <div class="col col-3">
-        <!--sellest teeb eraldi komponendi "StorageImage.vue"-->
-        <img src="img_placeholder.jpg" class="img-thumbnail">
+        <!--siia tuleb eraldi komponent "StorageImage.vue"-->
+        <img src="@\assets\img_placeholder.jpg" class="img-thumbnail">
       </div>
 
-      <div class="col col-6">
-        <div class="row">
-          Maakond
-        </div>
-        <div class="row">
-          Vaata kaardil
-        </div>
-        <div class="row">
-          Suurus
-        </div>
-        <div class="row">
-          Tüüp
-        </div>
-        <div class="row">
-          Lisad
-        </div>
-        <div class="row">
-          Hind ühes kuus
-        </div>
+      <StorageDetailsInfoTable/>
+
+    </div>
+    <br>
+    <br>
+    <div class="row justify-content-center">
+      {{ storageDescription }}
+    </div>
+    <br>
+    <br>
+
+    <div>
+      <a v-if="isLoggedIn && isSeller||isAdmin" class="btn btn-primary">Salvesta</a>
+      <div v-else-if="isLoggedIn">
+        Algus: <input type="date" class="">
+        Lõpp: <input type="date" class="m-2">
+        <a class="btn btn-primary">Kontrolli saadavust</a>
       </div>
+      <a v-else href="/login" class="btn btn-primary">Tellimiseks logi sisse</a>
+      <a href="/alllocations" class="btn btn-primary m-2">Loobu</a>
     </div>
   </div>
 
 
 </template>
 
-
 <script>
 
-import StorageImage from "@/components/image/StorageImage.vue";
+import StorageDetailsInfoTable from "@/components/StorageDetailsInfoTable.vue";
 
 export default {
   name: "StorageDetailsView",
-  components: {StorageImage},
+  components: {StorageDetailsInfoTable},
 
-  data() {
+  data () {
     return {
-      storageId: '10'
+      isLoggedIn: false,
+      isAdmin: false,
+      isSeller: false,
 
+      storageName: 'Kohalik ladu 10',
+      storageDescription: "Täpsustav kirjeldus ja muu lisainfo",
+      startDate: '',
+      endDate: ''
     }
-  }
+  },
+
+  methods: {
+    getAndSetIsLoggedIn () {
+      const userId = sessionStorage.getItem('userId')
+      this.isLoggedIn = userId !== null
+    },
+
+    getAndSetIsSeller () {
+      const roleName = sessionStorage.getItem('roleName')
+      this.isSeller = roleName === 'seller'
+    },
+    getAndSetIsAdmin () {
+      const roleName = sessionStorage.getItem('roleName')
+      this.isAdmin = roleName === 'admin'
+    }
+  },
+  mounted () {
+    this.getAndSetIsLoggedIn()
+    this.getAndSetIsSeller()
+    this.getAndSetIsAdmin()
+  },
 
 }
-
 
 </script>
