bbd89a4a55e3ffaa15dae3f5875cfbf4a0c9921c
AK <a@b.com>
2023-12-07 22:29:44 +0200

    FES-2 counties ja type dropdowni sisu tuleb baasist

--- src/components/CountiesDropdown.vue
+++ src/components/CountiesDropdown.vue
@@ -1,17 +1,15 @@
 <template>
-
   <select class="form-select" aria-label="Maakond">
-  <option v-for="county in counties" :key="county.countyId" :value="county.countyName">{{ county.countyName }}</option>
-
-  <!--<select v-model="selectedCityId" @change="emitSelectedCityId" class="form-select">
-    <option selected value="0">Kõik linnad</option>
-  </select>
-
     <option selected value="0">Vali maakond</option>
-    <option value="1">One</option>  -->
+    <option v-for="county in counties" :key="county.countyId" :value="county.countyName">
+      {{county.countyName}}
+    </option>
+
+    <!--<select v-model="selectedCityId" @change="emitSelectedCityId" class="form-select">
+      <option selected value="0">Kõik linnad</option>
+    </select>-->
+
   </select>
-
-
 </template>
 
 
@@ -19,8 +17,8 @@
 export default {
   name: 'CountiesDropdown',
 
-  data(){
-    return{
+  data () {
+    return {
       selectedCountyId: 0,
       counties: [
         {
@@ -32,22 +30,22 @@ export default {
   },
 
 
-methods: {
-  getCounties() {
-    this.$http.get("/location/counties")
-        .then(response => {
-          this.counties = response.data
-        })
-        .catch(error => {
-          //const errorResponseBody = error.response.data
-        })
+  methods: {
+    getCounties () {
+      this.$http.get("/location/counties")
+          .then(response => {
+            this.counties = response.data
+          })
+          .catch(error => {
+            //const errorResponseBody = error.response.data
+          })
+    },
+
   },
 
-},
-
-mounted(){
+  mounted () {
     this.getCounties()
-}
+  }
 
 }
 </script>
--- src/components/StorageTypeDropdown.vue
+++ src/components/StorageTypeDropdown.vue
@@ -1,11 +1,43 @@
 <template>
+
   <select class="form-select" aria-label="Tüüp">
+
     <option selected value="0">Vali tüüp</option>
-    <option value="1">One</option>
+    <option v-for="type in types" :key="type.typeId" :value="type.typeName">{{type.typeName }}</option>
+
   </select>
 </template>
 <script>
 export default {
-  name: 'StorageTypeDropdown'
+  name: 'StorageTypeDropdown',
+
+  data(){
+    return{
+
+      selectedType: 0,
+      types: [
+        {
+          typeId: 0,
+          typeName: ''
+        }
+      ]
+
+    }
+  },
+  methods: {
+    getTypes () {
+      this.$http.get("/storage/types")
+          .then(response => {
+            this.types = response.data
+          })
+          .catch(error => {
+            //const errorResponseBody = error.response.data
+          })
+    },
+
+  },
+  mounted(){
+    this.getTypes()
+  }
 }
 </script>
