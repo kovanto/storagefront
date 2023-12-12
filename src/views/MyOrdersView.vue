1c6a40d30a732b02af5ddc7e4de3524f3ff37cca
Anu <irikanu@gmail.com>
2023-12-12 16:29:45 +0200

    MyProfileView wip

--- src/views/MyOrdersView.vue
+++ src/views/MyOrdersView.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <OrdersTable/>
+  <h1>Minu broneeringud</h1>
+  <div class="container text-center">
+    <div class="row" v-for="storageInfo in storageInfos" :key="storageInfo.userId">
+      <div class="col col-3">
+        <img :src="storageInfo.imageData" class="img-fluid img-thumbnail" width="400">
+      </div>
+      <div class="col col-6">
+
+      </div>
+      <div class="col col-3">
+        <div class="row"></div>
+        <div class="row"></div>
+      </div>
+    </div>
+    <button @click="navigateToAllLocationsView" type="submit" class="btn btn-outline-dark float-end">Vaata pakkumisi</button>
   </div>
 
 </template>
