b6d2a9f944804277b993d1cb98d133c404e752e9
Anu <irikanu@gmail.com>
2023-12-13 16:43:10 +0200

    MyOrdersView wip: cancel order, change order

---
+++ src/components/modal/ChangeOrderModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-if="isOpen" class="modal" tabindex="-1" style="display: block; background-blend-mode: lighten">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header border-0">
+          <h5 class="modal-title"></h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" @click="closeModal"></button>
+        </div>
+        <div class="modal-body border-0">
+          <h4>Muuda broneeringu kuupäevad</h4>
+        </div>
+        <div>
+          Algus: <input type="date" class="">
+          Lõpp: <input type="date" class="m-2">
+        </div>
+        <div class="modal-footer border-0">
+          <button type="button" class="btn btn-outline-dark me-auto" @click="closeModal">Loobu</button>
+          <button type="button" class="btn btn-outline-danger">Salvesta</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChangeOrderModal",
+  props: {
+    storageInfo: {}
+  },
+
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+
+  methods: {
+
+    closeModal() {
+      this.isOpen = false
+    },
+
+    openModal() {
+      this.isOpen = true
+    },
+
+  }
+}
+</script>
+
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -64,7 +64,7 @@ export default {
     handleIsAddOrUpdateUser() {
       if (useRoute().query.isEdit === 'true') {
         this.isAddUser = false
-        this.isUpdateUser = true;
+        this.isUpdateUser = true
         this.getProfileInfo()
       }
     },
